75a09c1e54e340c22cafcf85d76e533f219f358e
l123050891 <l123050891@gmail.com>
2021-04-09 22:34:42 +0800

    update seo

--- resources/views/layouts/template_en.blade.php
+++ resources/views/layouts/template_en.blade.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>正龍塑膠</title>
+    <?php $seo = App\Seo::find(1); ?>
+    <meta name="keywords" content="{{$seo->keyword}}">
+    <meta name="description" content="{{$seo->description}}" />
+    <title>{{$seo->title}}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
