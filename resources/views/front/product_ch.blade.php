a1c5d5ba8a39f24be63267596b6aed93fee1d663
l123050891 <l123050891@gmail.com>
2021-04-15 21:59:11 +0800

    update lightbox

--- resources/views/front/product_ch.blade.php
+++ resources/views/front/product_ch.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
 <link rel="stylesheet" href="/css/product.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" integrity="sha512-ZKX+BvQihRJPA8CROKBhDNvoc2aDMOdAlcm7TUQY+35XYtrd3yh95QOOhsPDQY9QnKE0Wqag9y38OIgEvb88cA==" crossorigin="anonymous" />
 
 {{-- <style>
     main #news .container .link .news_pic {
@@ -26,7 +27,7 @@
                 <div class="row">
                     <div class="product_thumbnail col-5 col-md-3">
                         <div class="product_pic">
-                            <img width="100%" class="lazy" data-src="{{$product->img}}" src="{{$product->img}}" alt="">
+                            <a href="{{$product->img}}" data-lightbox="product" data-title="{{$product->name_ch}}"><img width="100%" class="lazy" data-src="{{$product->img}}" src="{{$product->img}}" alt=""></a>
                         </div>
                         <div class="product_name">
                             {{$product->name_ch}}
@@ -50,5 +51,7 @@
 
 @section('js')
     <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.3.1/dist/lazyload.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js" integrity="sha512-k2GFCTbp9rQU412BStrcD/rlwv1PYec9SNrkbQlo6RZCf75l6KcC3UwDY8H5n5hl4v77IDtIPwOk9Dqjs/mMBQ==" crossorigin="anonymous"></script>
 
 @endsection
